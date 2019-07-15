32c8252d03fbd556c522778fe6636003bd8fa4c9
Dana Goin <danaegoin@gmail.com>
2019-07-15 15:41:42 -0700

    Fixed race variable

--- CiOB-ECHO data prep.R
+++ CiOB-ECHO data prep.R
@@ -10,8 +10,8 @@ df$mat_edu <- factor(df$mat_edu, levels=c(0,1,2,3,4,5),
 
 
 df$mat_race <- ifelse(df$race_m>=97,NA, df$race_m)
-df$mat_race <- factor(df$mat_race, levels=c(1,2,3,4,5,6), 
-                      labels=c("Asian","Pacific Islander","Black","White","Native American","Other"))
+#df$mat_race <- factor(df$mat_race, levels=c(1,2,3,4,5,6), 
+#                      labels=c("Asian","Pacific Islander","Black","White","Native American","Other"))
 
 df$mat_eth <- ifelse(df$latina_m>=97,NA, df$latina_m)
 
@@ -20,3 +20,6 @@ df$mat_race_eth <- ifelse(df$mat_eth==1, 1,
                                  ifelse(df$mat_eth==0 & df$mat_race==3, 3, 
                                         ifelse(df$mat_eth==0 & df$mat_race==4, 4, 
                                                ifelse(df$mat_eth==0 & (df$mat_race==5 | df$mat_race==6), 5, NA)))))
+
+df$mat_race_eth <- factor(df$mat_race_eth, levels=c(1,2,3,4,5), 
+                          labels=c("Latina","Asian/PI", "Black","White","Other or multiple"))
