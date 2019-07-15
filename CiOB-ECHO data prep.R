65f00fe1300e9301e828d651cea7d429865645c8
Dana Goin <danaegoin@gmail.com>
2019-07-15 15:26:34 -0700

    Read in data and recode variables

---
+++ CiOB-ECHO data prep.R
@@ -0,0 +1,22 @@
+# read in CiOB-ECHO data 
+
+df <- read.csv("/Users/danagoin/Documents/CiOB-ECHO/CiOB2/questionnaire.csv")
+
+# recode key variables  
+                   
+df$mat_edu <- ifelse(df$edu_m>=97, NA, df$edu_m)                   
+df$mat_edu <- factor(df$mat_edu, levels=c(0,1,2,3,4,5), 
+                   labels=c("Less than high school","High school grad","Some college","College grad","Master's degree","Doctoral degree"))
+
+
+df$mat_race <- ifelse(df$race_m>=97,NA, df$race_m)
+df$mat_race <- factor(df$mat_race, levels=c(1,2,3,4,5,6), 
+                      labels=c("Asian","Pacific Islander","Black","White","Native American","Other"))
+
+df$mat_eth <- ifelse(df$latina_m>=97,NA, df$latina_m)
+
+df$mat_race_eth <- ifelse(df$mat_eth==1, 1, 
+                          ifelse(df$mat_eth==0 & (df$mat_race==1 | df$mat_race==2), 2, 
+                                 ifelse(df$mat_eth==0 & df$mat_race==3, 3, 
+                                        ifelse(df$mat_eth==0 & df$mat_race==4, 4, 
+                                               ifelse(df$mat_eth==0 & (df$mat_race==5 | df$mat_race==6), 5, NA)))))
